7b0e84aa3dfdc2d8243525bc33a87cc302e26d7b
masa-dj <masadjelic@gmail.com>
2026-04-08 11:08:53 +0200

    Dockerization of the project
    For now its 3 containers: backend, frontend and database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PassportModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, 
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
